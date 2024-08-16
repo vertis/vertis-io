8590f4a520fdaa8c29086535a8997cb472f2efa7
Luke Chadwick <luke@vertis.io>
2024-08-16 08:46:12 +0100

    chore: this is a first pass at markdown

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+export function formatDate(date: string | Date): string {
+	const d = new Date(date);
+	return d.toLocaleDateString('en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
+}
---
+++ src/posts/hello.md
@@ -0,0 +1,11 @@
+---
+title: Hello
+description: Hello post.
+date: '2024-08-14'
+categories:
+  - sveltekit
+  - svelte
+published: true
+---
+
+# Hello
